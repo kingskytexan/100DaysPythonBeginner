14db2dabd6f3f868b87c6b21fb2904f30814cceb
Tommy Klapheke <tommyklapheke@yahoo.com>
2023-01-09 22:04:06 -0500

    Created the Band Name Generaotr. Takes input form the user for birth city and pet name. Uses these to create band name for the user.

---
+++ Day1/main.py
@@ -0,0 +1,10 @@
+# Band Name Generator program
+print("Welcome to the Band Name Generator!!")
+print("Where you will be able to create your own unique band name.")
+
+# Take input from the user
+birth_city = input("What is the city you were born in? ")
+pet_name = input("What is the name of your pet? ")
+
+# Prints their band name
+print("This is your band name: " +birth_city + " " + pet_name)
